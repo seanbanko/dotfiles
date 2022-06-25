85672e8fc1bc215f2b0ee6dd2ea38d594b1649d8
Sean Banko <sean.banko@gmail.com>
2022-06-25 16:07:06 -0700

    Check for macOS before doing macOS things

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,14 +7,28 @@ cd ~
 # Install Homebrew so that you can use 
 
 echo "Installing Homebrew"
-/bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+# Add appropriate version of brew to PATH
+if test ! "$(uname)" = "Darwin"
+then
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /home/sean/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv shellenv)"
+else
+    echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/sean/.zprofile
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+fi
 
 # Install packages
 
 echo "Installing packages"
-sudo apt update && sudo apt upgrade
-brew install firefox git kitty nvim spotify zsh
+brew install git glow nvim ripgrep spotify tree zsh
+brew install --cask kitty
+
+# Install Firefox (only available on macOS Homebrew)
+if test ! "$(uname)" = "Darwin"
+  then
+fi
 
 # Install dotfiles
 
@@ -24,17 +38,18 @@ git clone https://github.com/seanbanko/dotfiles && dotfiles/install
 # Install oh-my-zsh
 
 echo "Installing oh-my-zsh"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+/bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 chsh -s $(which zsh)
 
-# GCM Core
-
-echo "Installing GCM Core"
-curl -sSL https://packages.microsoft.com/config/ubuntu/21.04/prod.list | sudo tee /etc/apt/sources.list.d/microsoft-prod.list
-curl -sSL https://packages.microsoft.com/keys/microsoft.asc | sudo tee /etc/apt/trusted.gpg.d/microsoft.asc
-sudo apt-get update
-sudo apt-get install gcmcore
-git-credential-manager-core configure
+# Re-add Homebrew to PATH because it probably got removed
+if test ! "$(uname)" = "Darwin"
+then
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /home/sean/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv shellenv)"
+else
+    echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/sean/.zprofile
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+fi
